f76724e9a7e95f766f50e1ca698f57ca0ad4b9bc
Phamtien <phamtien1709@gmail.com>
2017-09-29 10:43:30 +0700

    FE lv1

---
+++ server.js
@@ -0,0 +1,26 @@
+const express = require('express');
+const bodyParser = require('body-parser');
+const mongoose = require('mongoose');
+const config = require('./config.json');
+
+const homeRouter = require('./homeRouter');
+
+let app = express();
+
+mongoose.connect(config.connectionString, (err) => {
+  if (err) {
+    console.log(err);
+  } else {
+    console.log('connect success');
+  }
+});
+
+app.use(bodyParser.urlencoded({ extended: true}));
+
+app.use('/', homeRouter);
+
+app.use(express.static(__dirname + '/public'));
+
+app.listen(6969, () => {
+  console.log('Server is ready at port', config.port);
+});
